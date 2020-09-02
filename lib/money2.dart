d9adb7cdb5d38f4b20df3dad60c741569748602a
Patrick Teufel <patrick.teufel@datev.de>
2020-09-02 16:20:23 +0200

    export encoding functionality

--- lib/money2.dart
+++ lib/money2.dart
@@ -92,3 +92,5 @@ export 'src/common_currencies.dart';
 export 'src/currencies.dart';
 export 'src/currency.dart';
 export 'src/money.dart';
+export 'src/encoders.dart';
+export 'src/money_data.dart';
